f79b46369b9f9063ce15cf0a0b28883a1bb3d360
chandima2000 <hychandima2000@gmail.com>
2024-11-02 17:30:55 +0530

    Fetch a particular Quiz from the DB

--- src/main/java/org/chandima/quizapp/controller/QuizController.java
+++ src/main/java/org/chandima/quizapp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package org.chandima.quizapp.controller;
 
+
+import org.chandima.quizapp.model.QuestionWrapper;
 import org.chandima.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -15,9 +16,17 @@ public class QuizController {
 
     @Autowired
     private QuizService quizService;
+
+
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
 
         return  quizService.createQuiz(category, numQ, title);
+    }
+
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable("id") Integer quizId){
+        return quizService.getQuiz(quizId);
     }
 }
--- src/main/java/org/chandima/quizapp/service/QuizService.java
+++ src/main/java/org/chandima/quizapp/service/QuizService.java
@@ -1,6 +1,7 @@
 package org.chandima.quizapp.service;
 
 import org.chandima.quizapp.model.Question;
+import org.chandima.quizapp.model.QuestionWrapper;
 import org.chandima.quizapp.model.Quiz;
 import org.chandima.quizapp.repository.QuestionRepo;
 import org.chandima.quizapp.repository.QuizRepo;
@@ -9,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +34,21 @@ public class QuizService {
         quizRepo.save(quiz);
 
         return new ResponseEntity<>("Quiz Created", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(int quizId) {
+
+            Optional<Quiz> quiz = quizRepo.findById(quizId);
+            List<Question> questionsFromDb = quiz.get().getQuestions();
+            List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+            for(Question q : questionsFromDb){
+                QuestionWrapper questionWrapper = new QuestionWrapper(q.getId(),q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+
+                questionsForUser.add(questionWrapper);
+            }
+
+            return new ResponseEntity<>(questionsForUser,HttpStatus.OK);
+
     }
 }
